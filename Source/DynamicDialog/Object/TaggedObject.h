5d9eb1abed33f2f09c45b0c9dadcddf0e3ff8f2f
Niklas <niklas@niklasandersson.net>
2015-09-23 14:58:34 +0200

    Add Tagged object class

---
+++ Source/DynamicDialog/Object/TaggedObject.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "TaggedObject.generated.h"
+
+UCLASS()
+class DYNAMICDIALOG_API ATaggedObject : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ATaggedObject();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	
+	
+};
